80fd82226cef2905384b7a5d674642bebfb1d568
anastasia.yarotskas <nastyayarotskas@gmail.com>
2025-06-04 20:55:14 +0300

    Add auth

--- front-ui/src/main/java/ru/yandex/practicum/front/ui/feature/cash/CashServiceClient.java
+++ front-ui/src/main/java/ru/yandex/practicum/front/ui/feature/cash/CashServiceClient.java
@@ -1,6 +1,12 @@
 package ru.yandex.practicum.front.ui.feature.cash;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,16 +15,31 @@ import reactor.core.publisher.Mono;
 public class CashServiceClient {
 
     private final WebClient cashServiceWebClient;
+    @Autowired
+    private ReactiveOAuth2AuthorizedClientManager manager;
 
     public CashServiceClient(@LoadBalanced WebClient.Builder cashServiceWebClientBuilder) {
         this.cashServiceWebClient = cashServiceWebClientBuilder.build();
     }
 
     public Mono<Void> processAccountTransaction(String login, CashChangeRequest request) {
-        return cashServiceWebClient.post()
-                .uri("/api/v1/users/" + login + "/cash")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(Void.class);
+        return retrieveToken()
+                .flatMap(
+                        accessToken -> cashServiceWebClient.post()
+                                .uri("/api/v1/users/" + login + "/cash")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                                .bodyValue(request)
+                                .retrieve()
+                                .bodyToMono(Void.class)
+                );
+    }
+
+    private Mono<String> retrieveToken() {
+        return manager.authorize(OAuth2AuthorizeRequest
+                        .withClientRegistrationId("front-ui-client")
+                        .principal("system")
+                        .build())
+                .map(OAuth2AuthorizedClient::getAccessToken)
+                .map(OAuth2AccessToken::getTokenValue);
     }
 }
--- front-ui/src/main/java/ru/yandex/practicum/front/ui/feature/transfer/TransferServiceClient.java
+++ front-ui/src/main/java/ru/yandex/practicum/front/ui/feature/transfer/TransferServiceClient.java
@@ -1,6 +1,12 @@
 package ru.yandex.practicum.front.ui.feature.transfer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,6 +15,8 @@ import reactor.core.publisher.Mono;
 public class TransferServiceClient {
 
     private final WebClient transferServiceWebClient;
+    @Autowired
+    private ReactiveOAuth2AuthorizedClientManager manager;
 
     public TransferServiceClient(@LoadBalanced WebClient.Builder transferServiceWebClientBuilder) {
         this.transferServiceWebClient = transferServiceWebClientBuilder.build();
@@ -16,10 +24,23 @@ public class TransferServiceClient {
 
     public Mono<Void> transfer(String login, TransferRequest request) {
         request.setLogin(login);
-        return transferServiceWebClient.post()
-                .uri("/users/" + login + "/transfer")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(Void.class);
+        return retrieveToken()
+                .flatMap(
+                        accessToken -> transferServiceWebClient.post()
+                                .uri("/users/" + login + "/transfer")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                                .bodyValue(request)
+                                .retrieve()
+                                .bodyToMono(Void.class)
+                );
+    }
+
+    private Mono<String> retrieveToken() {
+        return manager.authorize(OAuth2AuthorizeRequest
+                        .withClientRegistrationId("front-ui-client")
+                        .principal("system")
+                        .build())
+                .map(OAuth2AuthorizedClient::getAccessToken)
+                .map(OAuth2AccessToken::getTokenValue);
     }
 }
