fd081b14460bb242fdab0192fed7673bb37fd9b2
anastasia.yarotskas <nastyayarotskas@gmail.com>
2025-07-17 17:22:44 +0300

    Add notification failure metric

--- notification-service/src/main/java/ru/yandex/practicum/notification/service/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/ru/yandex/practicum/notification/service/consumer/NotificationConsumer.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.notification.service.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -15,10 +16,17 @@ import ru.yandex.practicum.model.NotificationRequest;
 public class NotificationConsumer {
 
     private final SimpMessagingTemplate messagingTemplate;
+    @Autowired
+    private NotificationMetrics notificationMetrics;
 
     @KafkaListener(topics = "notifications", groupId = "notification-service")
     public void listen(@Payload NotificationRequest request) {
         log.info("Got notification: {}", request);
-        messagingTemplate.convertAndSend("/topic/notifications", request);
+        try {
+            messagingTemplate.convertAndSend("/topic/notifications", request);
+        } catch (Exception e) {
+            log.error("Failed to send notification to user {}: {}", request.getLogin(), e.getMessage());
+            notificationMetrics.incrementNotificationFailure(request.getLogin());
+        }
     }
 }
